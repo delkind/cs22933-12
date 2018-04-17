5ff0d93f9773eb2e3d8acec39953ed6f52148db5
assaflehr <assaflehr@gmail.com>
2018-04-17 16:12:15 +0300

    adding explore&setup utilities, and changing order 0 training
    ugly loop, cause I did not find an easy way to it (probably there is
    one, but , for lack of time...)

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='tagger',
+   version='1.0',
+   description='part-of-speech tagger project using Hidden Markov Model',
+   author_email='hidden_email',
+   #packages=['tagger'],  #same as name
+   install_requires=['numpy', ]
+)
