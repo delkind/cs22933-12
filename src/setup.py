e2d54deda070c53ef1aee887af438daf2e442c07
David Elkind <david.elkind@gmail.com>
2018-04-17 23:57:38 +0300

    Capitalized names

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ setup(
    name='tagger',
    version='1.0',
    description='part-of-speech tagger project using Hidden Markov Model',
-   author='David elkind, assaf lehr',
+   author='David Elkind, Assaf Lehr',
    author_email='hidden_email',
    #packages=['tagger'],  #same as name
    install_requires=['numpy', ]
