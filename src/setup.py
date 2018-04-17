eeff34d9bbf16eeabbc3dd189e6e5273b3a34c43
assaflehr <assaflehr@gmail.com>
2018-04-17 16:12:25 +0300

    setup

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,7 @@ setup(
    name='tagger',
    version='1.0',
    description='part-of-speech tagger project using Hidden Markov Model',
+   author='David elkind, assaf lehr',
    author_email='hidden_email',
    #packages=['tagger'],  #same as name
    install_requires=['numpy', ]
